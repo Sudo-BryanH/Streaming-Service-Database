e8c604421914afdce1decc975dacc00d92aa4b68
Bryan H <bryanhui77@hotmail.com>
2023-03-20 17:29:35 -0700

    Fixed the syntactical errors in the INSERT statements and added some more tuples to billing address and postal code

--- src/sql/scripts/databaseSetup.sql
+++ src/sql/scripts/databaseSetup.sql
@@ -199,391 +199,273 @@ CREATE TABLE Creates(
 
 INSERT INTO Release VALUES
 
-                        (1, 'Whenever You Need Somebody', 'Album', 1987-11-12, 	 'https://cdn.spotube.com/img/rickroll.png', 'BMG Rights Management (UK) Limited'),
+INSERT INTO Release VALUES (1, 'Whenever You Need Somebody', 'Album', 1987-11-12, 	 'https://cdn.spotube.com/img/rickroll.png', 'BMG Rights Management (UK) Limited');
 
-                        (2, 'Astro Lounge', 'Album', 1999-06-08, 'https://cdn.spotube.com/img/shrek.png', 'Interscope Geffen (A&M) Records'),
+INSERT INTO Release VALUES (2, 'Astro Lounge', 'Album', 1999-06-08, 'https://cdn.spotube.com/img/shrek.png', 'Interscope Geffen (A&M) Records');
 
-                        (3, 'Curb Your Enthusiasm (Music From the TV Series)', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/frolic.png', 'Mellowdrama Records'),
+INSERT INTO Release VALUES (3, 'Curb Your Enthusiasm (Music From the TV Series)', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/frolic.png', 'Mellowdrama Records');
 
-                        (4, 'Tritio Jatra', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/nemesis.png', 'Studio Bangi'),
+INSERT INTO Release VALUES (4, 'Tritio Jatra', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/nemesis.png', 'Studio Bangi');
 
-                        (5, 'Superunknown', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/soundgarden.png', 'Sony Music');
+INSERT INTO Release VALUES(5, 'Superunknown', 'Album', 2006-04-17, 'https://cdn.spotube.com/img/soundgarden.png', 'Sony Music');
 
 
 
 INSERT INTO Song VALUES
 
-                     (1, 1, 'Never Gonna Give You Up', 214, 'Pop', 213422997),
+INSERT INTO Song VALUES(1, 1, 'Never Gonna Give You Up', 214, 'Pop', 213422997);
 
-                     (1, 2, 'Whenever You Need Somebody', 243, 'Pop', 1842825),
+INSERT INTO Song VALUES(1, 2, 'Whenever You Need Somebody', 243, 'Pop', 1842825);
 
-                     (2, 4, 'All Star', 190, 'Alternative', 692894289),
+INSERT INTO Song VALUES (2, 4, 'All Star', 190, 'Alternative', 692894289);
 
-                     (3, 1, 'Frolic', 210, 'Soundtrack', 5359039),
+INSERT INTO Song VALUES(3, 1, 'Frolic', 210, 'Soundtrack', 5359039);
 
-                     (4, 4, 'Nirbashon', 210, 'Rock', 5359039);
+INSERT INTO Song VALUES (4, 4, 'Nirbashon', 210, 'Rock', 5359039);
 
 
 
-INSERT INTO Artist VALUES
 
-                       (1, 'Rick Astley', 5829256),
 
-                       (2, 'Smash Mouth', 3950387),
+INSERT INTO Artist VALUES (1, 'Rick Astley', 5829256);
 
-                       (3, 'Curb Your Enthusiasm', 38358),
+INSERT INTO Artist VALUES  (2, 'Smash Mouth', 3950387);
 
-                       (4, 'Luciano Michelini', 35294);
+INSERT INTO Artist VALUES  (3, 'Curb Your Enthusiasm', 38358);
+INSERT INTO Artist VALUES (4, 'Luciano Michelini', 35294);
 
-(5, 'Nemesis', 200000);
+INSERT INTO Artist VALUES (5, 'Nemesis', 200000);
 
 
 
 INSERT INTO Distributor VALUES
 
-                            ('BMG Rights Management (UK) Limited', 'https://www.bmg.com/'),
+INSERT INTO Distributor VALUES ('BMG Rights Management (UK) Limited', 'https://www.bmg.com/');
 
-                            ('Interscope Geffen (A&M) Records', 'https://www.interscope.com'),
+INSERT INTO Distributor VALUES ('Interscope Geffen (A&M) Records', 'https://www.interscope.com');
 
-                            ('Mellowdrama Records', NULL);
+INSERT INTO Distributor VALUES ('Mellowdrama Records', NULL);
 
-('Studio Bangi', ' https://www.studio-bangi.com');
+INSERT INTO Distributor VALUES ('Studio Bangi', ' https://www.studio-bangi.com');
 
-('Sony Music', 'https://www.sonymusic.com');
+INSERT INTO Distributor VALUES ('Sony Music', 'https://www.sonymusic.com');
 
 
 
 INSERT INTO UserPayment VALUES
 
-                            (1, 7, 2022-03-01, 2394235323332438, 'bryanh', 3235, 'Joyce st.', 'V6E7F3'),
+INSERT INTO UserPayment VALUES (1, 7, 2022-03-01, 2394235323332438, 'bryanh', 3235, 'Joyce st.', 'V6E7F3');
 
-                            (2, 11, 2022-03-01, 5434235323332438, 'sleepy', 1234, 'Joyce st.', 'V6E7F3'),
+INSERT INTO UserPayment VALUES (2, 11, 2022-03-01, 5434235323332438, 'sleepy', 1234, 'Joyce st.', 'V6E7F3');
 
-                            (3, 12, 2022-02-01, 1234353233324332, 'A113', 1234, 'Cambie st.', 'V6E7F3'),
+INSERT INTO UserPayment VALUES (3, 12, 2022-02-01, 1234353233324332, 'A113', 1234, 'Cambie st.', 'V6E7F3');
 
-                            (4, 15, 2022-02-01, 7893353233324342, 'notstevejobs', 1234, 'Cambie st.', 'V4E7F3'),
+INSERT INTO UserPayment VALUES (4, 15, 2022-02-01, 7893353233324342, 'notstevejobs', 1234, 'Cambie st.', 'V4E7F3');
 
-                            (5, 15, 2022-02-01, 8989353233324023, 'celloist', 7843, 'Cambie st.', 'V6E7F3');
+INSERT INTO UserPayment VALUES (5, 15, 2022-02-01, 8989353233324023, 'celloist', 7843, 'Cambie st.', 'V6E7F3');
 
 
 
-INSERT INTO CardTable VALUES
 
-                          ('Visa', 2394235323332438, 2025-01-01),
 
-                          ('Amex', 5434235323332438, 2026-01-01),
+INSERT INTO CardTable VALUES ('Visa', 2394235323332438, 2025-01-01);
 
-                          ('Mastercard', 1234353233324332, 2027-01-01),
+INSERT INTO CardTable VALUES ('Amex', 5434235323332438, 2026-01-01);
 
-                          ('Visa', 7893353233324342, 2025-02-01),
+INSERT INTO CardTable VALUES ('Mastercard', 1234353233324332, 2027-01-01);
 
-                          ('Amex', 8989353233324023, 2023-07-01);
+INSERT INTO CardTable VALUES ('Visa', 7893353233324342, 2025-02-01);
 
+INSERT INTO CardTable VALUES ('Amex', 8989353233324023, 2023-07-01);
 
 
-INSERT INTO CompanyPayment VALUES
 
-                               (1, 7000, 2022-03-01, 3235, 'Courtney St.', 'V6E5E3', 'Sony Music'),
 
-                               (2, 1100, 2022-03-01, 1234, 'Camosun St.', 'V6E789', 'Studio Bangi'),
 
-                               (3, 1200, 2022-02-01, 1234, 'Cambie st.', 'V8E7F3', 'BMG Rights Management (UK) Limited'),
+INSERT INTO CompanyPayment VALUES (1, 7000, 2022-03-01, 3235, 'Courtney St.', 'V6E5E3', 'Sony Music');
 
-                               (4, 1500, 2022-02-01, 1234, 'Hastings st.', 'V6E7K3', 'Interscope Geffen (A&M) Records'),
+INSERT INTO CompanyPayment VALUES (2, 1100, 2022-03-01, 1234, 'Camosun St.', 'V6E789', 'Studio Bangi');
 
-                               (5, 1500, 2022-02-01, 7843, 'Manitoba st.', 'V6E1K2', 'Mellowdrama Records');
+INSERT INTO CompanyPayment VALUES (3, 1200, 2022-02-01, 1234, 'Cambie st.', 'V8E7F3', 'BMG Rights Management (UK) Limited');
 
+INSERT INTO CompanyPayment VALUES (4, 1500, 2022-02-01, 1234, 'Hastings st.', 'V6E7K3', 'Interscope Geffen (A&M) Records');
 
+INSERT INTO CompanyPayment VALUES (5, 1500, 2022-02-01, 7843, 'Manitoba st.', 'V6E1K2', 'Mellowdrama Records');
 
-INSERT INTO BillingAddress VALUES
 
-                               (3235, 'Joyce st.', 'V6E7F3'),
 
-                               (1234, 'Joyce st.', 'V6E7F3'),
-                               (1234, 'Cambie st.', 'V4E7F3'),
 
-                               (2829, 'Cambie st.', 'V6E7F3'),
 
-                               (7843, 'Cambie st.', 'V6E7F3'),
+INSERT INTO BillingAddress VALUES (3235, 'Joyce st.', 'V6E7F3');
 
-                               (3235, 'Courtney St.', 'V6E5E3'),
+INSERT INTO BillingAddress VALUES (1234, 'Joyce st.', 'V6E7F3');
+INSERT INTO BillingAddress VALUES (1234, 'Cambie st.', 'V4E7F3');
 
-                               (1234, 'Camosun St.', 'V6E789'),
+INSERT INTO BillingAddress VALUES (2829, 'Cambie st.', 'V6E7F3');
 
-                               (2948, 'Cambie st.', 'V4E7F3'),
+INSERT INTO BillingAddress VALUES (7843, 'Cambie st.', 'V6E7F3');
 
-                               (1234, 'Hastings st.', 'V8E7K3',),
+INSERT INTO BillingAddress VALUES (3235, 'Courtney St.', 'V6E5E3');
 
-                               (7843, 'Manitoba st.', 'V6E1K2');
+INSERT INTO BillingAddress VALUES (1234, 'Camosun St.', 'V6E789');
 
+INSERT INTO BillingAddress VALUES (2948, 'Cambie st.', 'V4E7F3');
 
+INSERT INTO BillingAddress VALUES (1234, 'Hastings st.', 'V8E7K3',);
 
-INSERT INTO PostalCodeCityProvince VALUES
+INSERT INTO BillingAddress VALUES (7843, 'Manitoba st.', 'V6E1K2');
 
-                                       ('Vancouver', 'BC', 'V6E7F3'),
+INSERT INTO BillingAddress VALUES (2345, 'Kenneth st.', 'G5E5C6');
 
-                                       ('Vancouver', 'BC', 'V4E7F3'),
-                                       ('Vancouver', 'BC,' 'V6E5E3'),
-                                       ('Vancouver', 'BC', 'V6E789'),
-                                       ('Vancouver', 'BC', 'V8E7K3'),
+INSERT INTO BillingAddress VALUES (8343, 'Montgomery st.', 'H4R7J8');
 
-                                       ('Vancouver', 'BC', 'V6E7K3'),
+INSERT INTO BillingAddress VALUES (8583, 'Elliot st.', 'K9L0H1')
 
-                                       ('Vancouver', 'BC', 'V6E1K2');
 
 
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V6E7F3');
 
-INSERT INTO User
-VALUES ('bryanh', 'bryan@gmail.com', 'CS213Sucks', 2022 - 01 - 01);
-INSERT INTO User
-VALUES ('harperk', 'harper@gmail.com', 'CS310Sucks', 2022 - 02 - 02);
-INSERT INTO User
-VALUES ('dhrubok', 'dhrubo@hotmail.com', 'CS210Sucks', 2021 - 09 - 01);
-INSERT INTO User
-VALUES ('kimdol', 'kimdol@yahoo.com', 'CS304Sucks', 2022 - 03 - 04);
-INSERT INTO User
-VALUES ('A113, 'A113@outlook.com', 'CS304Sucks', 2022-09-10),
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V4E7F3');
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC,' 'V6E5E3');
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V6E789');
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V8E7K3');
 
-('eenie meenie', 'eenie@gmail.com', 'whatever', 2021-02-09),
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V6E7K3');
 
-('sleepy', 'sleepingInClass@gmail.com', 'allnighter', 2023-01-01),
+INSERT INTO PostalCodeCityProvince VALUES ('Vancouver', 'BC', 'V6E1K2');
 
-('celloist', 'cello-ing@yahoo.com', 'prez', 2021-04-06),
+INSERT INTO PostalCodeCityProvince VALUES ('Calgary', 'AB', 'G5E5C6');
 
-('notstevejobs', 'jobs@gmail.com', 'googlebest', 2020-01-09),
+INSERT INTO PostalCodeCityProvince VALUES ('Winnepeg', 'MB', 'H4R7J8');
 
-('jfk1975', 'jfk@yahoo.com', 'ishouldbedead', 2019-01-03);
+INSERT INTO PostalCodeCityProvince VALUES ('Toronto', 'ON', 'K9L0H1');
 
 
 
-INSERT INTO FreeUser VALUES
+INSERT INTO User VALUES ('eenie meenie', 'eenie@gmail.com', 'whatever', 2021-02-09);
 
-('harperk', 13),
+INSERT INTO User VALUES ('sleepy', 'sleepingInClass@gmail.com', 'allnighter', 2023-01-01);
 
-('dhrubok', 16),
+INSERT INTO User VALUES ('celloist', 'cello-ing@yahoo.com', 'prez', 2021-04-06);
 
-('kimdol', 0),
+INSERT INTO User VALUES ('notstevejobs', 'jobs@gmail.com', 'googlebest', 2020-01-09);
 
-('jfk1975', 13),
+INSERT INTO User VALUES ('jfk1975', 'jfk@yahoo.com', 'ishouldbedead', 2019-01-03);
 
-('eenie meenie', 6);
+INSERT INTO User VALUES ('bryanh', 'bryan@gmail.com', 'CS213Sucks', 2022 - 01 - 01);
 
+INSERT INTO User VALUES ('harperk', 'harper@gmail.com', 'CS310Sucks', 2022 - 02 - 02);
 
+INSERT INTO User VALUES ('dhrubok', 'dhrubo@hotmail.com', 'CS210Sucks', 2021 - 09 - 01);
 
-INSERT INTO PremiumUser VALUES
+INSERT INTO User VALUES ('kimdol', 'kimdol@yahoo.com', 'CS304Sucks', 2022 - 03 - 04);
 
-('bryanh', 2022-03-01, 2023-03-01),
+INSERT INTO User VALUES ('A113, 'A113@outlook.com', 'CS304Sucks', 2022-09-10);
 
-('A113', 2022-02-01, 2023-02-01),
 
-('sleepy', 2023-03-01, 2024-02-01),
 
-('celloist', 2023-03-01, 2024-02-01),
 
-('notstevejobs', 2021-03-01, 2022-02-01);
 
 
+INSERT INTO FreeUser VALUES ('harperk', 13);
 
-INSERT INTO Playlist VALUES
+INSERT INTO FreeUser VALUES ('dhrubok', 16);
 
-('bryanh','lofi'),
+INSERT INTO FreeUser VALUES ('kimdol', 0);
 
-('eenie meenie','90s jam',),
+INSERT INTO FreeUser VALUES ('jfk1975', 13);
 
-('kimdol','study-session'),
+INSERT INTO FreeUser VALUES ('eenie meenie', 6);
 
-('jfk1975','80s glam metal'),
-('A113','alt jam');
 
 
 
-INSERT INTO PlaylistIsIn VALUES
+INSERT INTO PremiumUser VALUES ('bryanh', 2022-03-01, 2023-03-01);
 
-('bryanh','lofi',1,1),
+INSERT INTO PremiumUser VALUES ('A113', 2022-02-01, 2023-02-01);
 
-('bryanh','lofi',1,2),
+INSERT INTO PremiumUser VALUES ('sleepy', 2023-03-01, 2024-02-01);
 
-('kimdol','study-session',2,4),
+INSERT INTO PremiumUser VALUES ('celloist', 2023-03-01, 2024-02-01);
 
-('eenie meenie',' 90s jam',4,4),
+INSERT INTO PremiumUser VALUES ('notstevejobs', 2021-03-01, 2022-02-01);
 
-('jfk1975','80s glam metal',3,1);
 
 
 
-INSERT INTO AddsToLibrary VALUES
 
-(1,1,'bryanh',0,1),
+INSERT INTO Playlist VALUES ('bryanh','lofi');
 
-(1,2,'bryanh',0,0),
+INSERT INTO Playlist VALUES ('eenie meenie','90s jam',);
 
-(2,4,'kimdol',0,1),
+INSERT INTO Playlist VALUES ('kimdol','study-session');
 
-(4,4,'sleepy',1,0),
-(3,1,'A113',1,1);
+INSERT INTO Playlist VALUES ('jfk1975','80s glam metal');
+INSERT INTO Playlist VALUES ('A113','alt jam');
 
 
 
-INSERT INTO FeaturedIn VALUES
 
-(1,1,1),
 
-(2,1,2),
+INSERT INTO PlaylistIsIn VALUES ('bryanh','lofi',1,1);
 
-(3,2,4),
+INSERT INTO PlaylistIsIn VALUES ('bryanh','lofi',1,2);
 
-(4,4,4),
+INSERT INTO PlaylistIsIn VALUES ('kimdol','study-session',2,4);
 
-(5,3,1);
+INSERT INTO PlaylistIsIn VALUES ('eenie meenie',' 90s jam',4,4);
 
+INSERT INTO PlaylistIsIn VALUES ('jfk1975','80s glam metal',3,1);
 
 
-INSERT INTO Follows VALUES,
 
-('bryanh',1),
 
-('kimdol',2),
 
-('eenie meenie',3),
+INSERT INTO AddsToLibrary VALUES (1,1,'bryanh',0,1);
 
-('A113','notstevejobs',2),
+INSERT INTO AddsToLibrary VALUES (1,2,'bryanh',0,0);
 
-('sleepy',5);
+INSERT INTO AddsToLibrary VALUES (2,4,'kimdol',0,1);
 
+INSERT INTO AddsToLibrary VALUES (4,4,'sleepy',1,0);
+INSERT INTO AddsToLibrary VALUES (3,1,'A113',1,1);
 
 
-INSERT INTO Creates VALUES
 
-(1,1),
 
-(2,2),
 
-(3,3),
+INSERT INTO FeaturedIn VALUES (1,1,1);
 
-(4,4),
+INSERT INTO FeaturedIn VALUES (2,1,2);
 
-(5,5);
+INSERT INTO FeaturedIn VALUES (3,2,4);
 
-  ||
-    ''A113@outlook.com', 'CS304Sucks', 2022-09-10),
+INSERT INTO FeaturedIn VALUES (4,4,4);
 
-('eenie meenie', 'eenie@gmail.com', 'whatever', 2021-02-09),
+INSERT INTO FeaturedIn VALUES (5,3,1);
 
-('sleepy', 'sleepingInClass@gmail.com', 'allnighter', 2023-01-01),
 
-('celloist', 'cello-ing@yahoo.com', 'prez', 2021-04-06),
 
-('notstevejobs', 'jobs@gmail.com', 'googlebest', 2020-01-09),
 
-('jfk1975', 'jfk@yahoo.com', 'ishouldbedead', 2019-01-03);
+INSERT INTO Follows VALUES ('bryanh',1);
 
+INSERT INTO Follows VALUES ('kimdol',2);
 
+INSERT INTO Follows VALUES ('eenie meenie',3);
 
-INSERT INTO FreeUser VALUES
+INSERT INTO Follows VALUES ('A113','notstevejobs',2);
 
-('harperk', 13),
+INSERT INTO Follows VALUES ('sleepy',5);
 
-('dhrubok', 16),
 
-('kimdol', 0),
 
-('jfk1975', 13),
+INSERT INTO Creates VALUES (1,1);
 
-('eenie meenie', 6);
+INSERT INTO Creates VALUES (2,2);
 
+INSERT INTO Creates VALUES (3,3);
 
+INSERT INTO Creates VALUES(4,4);
 
-INSERT INTO PremiumUser VALUES
-
-('bryanh', 2022-03-01, 2023-03-01),
-
-('A113', 2022-02-01, 2023-02-01),
-
-('sleepy', 2023-03-01, 2024-02-01),
-
-('celloist', 2023-03-01, 2024-02-01),
-
-('notstevejobs', 2021-03-01, 2022-02-01);
-
-
-
-INSERT INTO Playlist VALUES
-
-('bryanh','lofi'),
-
-('eenie meenie','90s jam',),
-
-('kimdol','study-session'),
-
-('jfk1975','80s glam metal'),
-('A113','alt jam');
-
-
-
-INSERT INTO PlaylistIsIn VALUES
-
-('bryanh','lofi',1,1),
-
-('bryanh','lofi',1,2),
-
-('kimdol','study-session',2,4),
-
-('eenie meenie',' 90s jam',4,4),
-
-('jfk1975','80s glam metal',3,1);
-
-
-
-INSERT INTO AddsToLibrary VALUES
-
-(1,1,'bryanh',0,1),
-
-(1,2,'bryanh',0,0),
-
-(2,4,'kimdol',0,1),
-
-(4,4,'sleepy',1,0),
-(3,1,'A113',1,1);
-
-
-
-INSERT INTO FeaturedIn VALUES
-
-(1,1,1),
-
-(2,1,2),
-
-(3,2,4),
-
-(4,4,4),
-
-(5,3,1);
-
-
-
-INSERT INTO Follows VALUES,
-
-('bryanh',1),
-
-('kimdol',2),
-
-('eenie meenie',3),
-
-('A113',2),
-
-('sleepy',5);
-
-
-
-INSERT INTO Creates VALUES
-
-(1,1),
-
-(2,2),
-
-(3,3),
-
-(4,4),
-
-(5,5);
+INSERT INTO Creates VALUES (5,5);
 
