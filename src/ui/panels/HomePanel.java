a51bbf12a6c1e4ee3833dca416ce7f0f8ed18fcd
lodmik <kimdol@proton.me>
2023-03-27 14:56:23 -0700

    Init HomePanel

--- src/ui/panels/HomePanel.java
+++ src/ui/panels/HomePanel.java
@@ -2,6 +2,9 @@ package ui.panels;
 
 import ui.MainUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class HomePanel extends ContentPanel{
     public HomePanel(MainUI mainUI) {
         super(mainUI);
@@ -9,6 +12,9 @@ public class HomePanel extends ContentPanel{
 
     @Override
     protected void generate() {
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
+        add(createTitle("Home"), BorderLayout.NORTH);
     }
 }
