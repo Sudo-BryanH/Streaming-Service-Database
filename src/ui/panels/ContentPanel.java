5cf69975f7287f0abc87a178ab51764714009cf2
lodmik <kimdol@proton.me>
2023-03-27 14:56:14 -0700

    Pull up commonly used title generator

--- src/ui/panels/ContentPanel.java
+++ src/ui/panels/ContentPanel.java
@@ -3,6 +3,7 @@ package ui.panels;
 import ui.MainUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class ContentPanel extends JPanel {
     protected MainUI mainUI;
@@ -13,4 +14,13 @@ public abstract class ContentPanel extends JPanel {
     }
 
     protected abstract void generate();
+
+    protected JLabel createTitle(String title) {
+        JLabel label = new JLabel(title);
+        Font font = label.getFont();
+        Font boldFont = new Font(font.getFontName(), Font.BOLD, 30);
+        label.setFont(boldFont);
+        label.setPreferredSize(new Dimension(5,40));
+        return label;
+    }
 }
