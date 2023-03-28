52a1b14571b7e5ca3554704e1b7ac48d8cbed310
Dhrubo <dhruv.karmaker@gmail.com>
2023-03-28 07:29:18 -0700

    refactored paymentspanel classes

--- src/ui/panels/AdminPaymentsPanel.java
+++ src/ui/panels/AdminPaymentsPanel.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 import static ui.panels.UserPaymentsPanel.createBoldedTitle;
 
-public class AdminPaymentsPanel extends ContentPanel{
+public class AdminPaymentsPanel extends PaymentPanel{
     private JPanel distributorsPanelTable;
     private ArrayList<String> distributors;
     private ArrayList<String> users;
@@ -131,49 +131,6 @@ public class AdminPaymentsPanel extends ContentPanel{
         panelTable.add(label, BorderLayout.NORTH);
         panelTable.add(scrollPane, BorderLayout.WEST);
         return panelTable;
-    }
-
-    private JPanel createPaymentPortalPanel() {
-        JPanel paymentPortalPanel = new JPanel(new GridLayout(10,2,8,10));
-        JLabel cardNumberLabel = new JLabel("Card Number:");
-        JTextField cardNumberTextField = new JTextField(20);
-        JLabel expDateLabel = new JLabel("Expiration Date:");
-        JTextField expDateTextField = new JTextField(20);
-        JLabel cvvLabel = new JLabel("CVV:");
-        JTextField cvvTextField = new JTextField(20);
-        JButton submitButton = new JButton("Submit");
-        paymentPortalPanel.add(cardNumberLabel);
-        paymentPortalPanel.add(cardNumberTextField);
-        paymentPortalPanel.add(expDateLabel);
-        paymentPortalPanel.add(expDateTextField);
-        paymentPortalPanel.add(cvvLabel);
-        paymentPortalPanel.add(cvvTextField);
-        JLabel BillingAddressLabel = createBoldedTitle("Billing Address");
-        paymentPortalPanel.add(BillingAddressLabel);
-        paymentPortalPanel.add(new JLabel());
-        JLabel streetNumberLabel = new JLabel("Street#:");
-        JTextField streetNumberTextField = new JTextField(20);
-        JLabel streetLabel = new JLabel("Street:");
-        JTextField streetTextField = new JTextField(20);
-        JLabel cityLabel = new JLabel("City:");
-        JTextField cityTextField = new JTextField(20);
-        JLabel provinceLabel = new JLabel("Province:");
-        JTextField provinceTextField = new JTextField(20);
-        JLabel postalCodeLabel = new JLabel("Postal Code:");
-        JTextField postalCodeTextField = new JTextField(20);
-        paymentPortalPanel.add(streetNumberLabel);
-        paymentPortalPanel.add(streetNumberTextField);
-        paymentPortalPanel.add(streetLabel);
-        paymentPortalPanel.add(streetTextField);
-        paymentPortalPanel.add(cityLabel);
-        paymentPortalPanel.add(cityTextField);
-        paymentPortalPanel.add(provinceLabel);
-        paymentPortalPanel.add(provinceTextField);
-        paymentPortalPanel.add(postalCodeLabel);
-        paymentPortalPanel.add(postalCodeTextField);
-        paymentPortalPanel.add(new JLabel());
-        paymentPortalPanel.add(submitButton);
-        return paymentPortalPanel;
     }
 
     private void generateViewFrame(String user){
--- src/ui/panels/UserPaymentsPanel.java
+++ src/ui/panels/UserPaymentsPanel.java
@@ -9,7 +9,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UserPaymentsPanel extends ContentPanel{
+public class UserPaymentsPanel extends PaymentPanel{
     public UserPaymentsPanel(MainUI mainUI) {
         super(mainUI);
     }
@@ -32,48 +32,7 @@ public class UserPaymentsPanel extends ContentPanel{
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     }
 
-    private JPanel createPaymentPortalPanel() {
-        JPanel paymentPortalPanel = new JPanel(new GridLayout(10,2,8,10));
-        JLabel cardNumberLabel = new JLabel("Card Number:");
-        JTextField cardNumberTextField = new JTextField(20);
-        JLabel expDateLabel = new JLabel("Expiration Date:");
-        JTextField expDateTextField = new JTextField(20);
-        JLabel cvvLabel = new JLabel("CVV:");
-        JTextField cvvTextField = new JTextField(20);
-        JButton submitButton = new JButton("Submit");
-        paymentPortalPanel.add(cardNumberLabel);
-        paymentPortalPanel.add(cardNumberTextField);
-        paymentPortalPanel.add(expDateLabel);
-        paymentPortalPanel.add(expDateTextField);
-        paymentPortalPanel.add(cvvLabel);
-        paymentPortalPanel.add(cvvTextField);
-        JLabel BillingAddressLabel = createBoldedTitle("Billing Address");
-        paymentPortalPanel.add(BillingAddressLabel);
-        paymentPortalPanel.add(new JLabel());
-        JLabel streetNumberLabel = new JLabel("Street#:");
-        JTextField streetNumberTextField = new JTextField(20);
-        JLabel streetLabel = new JLabel("Street:");
-        JTextField streetTextField = new JTextField(20);
-        JLabel cityLabel = new JLabel("City:");
-        JTextField cityTextField = new JTextField(20);
-        JLabel provinceLabel = new JLabel("Province:");
-        JTextField provinceTextField = new JTextField(20);
-        JLabel postalCodeLabel = new JLabel("Postal Code:");
-        JTextField postalCodeTextField = new JTextField(20);
-        paymentPortalPanel.add(streetNumberLabel);
-        paymentPortalPanel.add(streetNumberTextField);
-        paymentPortalPanel.add(streetLabel);
-        paymentPortalPanel.add(streetTextField);
-        paymentPortalPanel.add(cityLabel);
-        paymentPortalPanel.add(cityTextField);
-        paymentPortalPanel.add(provinceLabel);
-        paymentPortalPanel.add(provinceTextField);
-        paymentPortalPanel.add(postalCodeLabel);
-        paymentPortalPanel.add(postalCodeTextField);
-        paymentPortalPanel.add(new JLabel());
-        paymentPortalPanel.add(submitButton);
-        return paymentPortalPanel;
-    }
+
 
    private JPanel createPayNowPanel(){
         JPanel payNowPanel = new JPanel();
