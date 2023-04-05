358516e3589970360e4437afccd0d14ff1278262
Dhrubo <dhruv.karmaker@gmail.com>
2023-04-05 11:18:05 -0700

    sanitization in login page

--- src/backend/LoginRegistrationEndpoints.java
+++ src/backend/LoginRegistrationEndpoints.java
@@ -3,10 +3,7 @@ package backend;
 import database.DatabaseManager;
 import util.PasswordHasher;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -15,10 +12,12 @@ public class LoginRegistrationEndpoints {
     public static boolean login(String username, String password){
         try {
             Connection connection = DatabaseManager.getInstance().getConnection();
-            Statement statement = connection.createStatement();
             password = PasswordHasher.hashPassword(password);
-            String query = "SELECT * FROM Users WHERE Username='" + username + "' AND Password='" + password + "'";
-            ResultSet resultSet = statement.executeQuery(query);
+            String query = "SELECT * FROM Users WHERE Username=? AND Password=?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,username);
+            statement.setString(2,password);
+            ResultSet resultSet = statement.executeQuery();
             boolean status = resultSet.next();
             return status;
         }
@@ -35,11 +34,18 @@ public class LoginRegistrationEndpoints {
         password = PasswordHasher.hashPassword(password);
         try{
             Connection connection = DatabaseManager.getInstance().getConnection();
-            Statement statement = connection.createStatement();
-            String query = String.format("INSERT INTO USERS VALUES('%s', '%s', '%s', '%s')",username,email,password,formattedDate);
-            statement.executeQuery(query);
-            query = String.format("INSERT INTO FreeUser VALUES('%s', %d)",username,0);
-            statement.executeQuery(query);
+            String query = "INSERT INTO USERS VALUES(?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,username);
+            statement.setString(2,email);
+            statement.setString(3,password);
+            statement.setString(4,formattedDate);
+            statement.executeQuery();
+            query = "INSERT INTO FreeUser VALUES(?, ?)";
+            statement = connection.prepareStatement(query);
+            statement.setString(1,username);
+            statement.setInt(2,0);
+            statement.executeQuery();
             return true;
         }
         catch (SQLException exception){
